9833943854085971a52b095a662cfadee431a31a
Jayron Martinez <jayronma@usc.edu>
2025-04-27 09:38:31 +0000

    First check

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,6 +21,8 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool helper(const AvailabilityMatrix& avail,const size_t dailyNeed, const size_t maxShifts,DailySchedule& sched, vector<size_t>& shiftsCount, size_t day, size_t slot);
+
 
 
 // Add your implementation of schedule() and other helper functions here
@@ -36,10 +38,43 @@ bool schedule(
         return false;
     }
     sched.clear();
-    // Add your code below
+    size_t n = avail.size();
+		size_t k= avail[0].size();
 
-
-
-
+		sched.assign(n,vector<Worker_T>(dailyNeed, INVALID_ID)); // schedule for n days with daill need slots
+		//track how many shifts per worker
+		vector<size_t> shiftsCount(k,0);
+		return helper(avail, dailyNeed, maxShifts, sched, shiftsCount, 0,0);//backtracking at day 0
 }
 
+
+bool helper(const AvailabilityMatrix& avail,const size_t dailyNeed, const size_t maxShifts,DailySchedule& sched, vector<size_t>& shiftsCount, size_t day, size_t slot){
+	size_t n = avail.size();
+	size_t k= avail[0].size();
+	if(day == n) return true; //all days scheduled
+	if(slot == dailyNeed){
+		return helper(avail, dailyNeed, maxShifts, sched, shiftsCount, day+1, 0);// move onto next day if today is filled
+	}
+	for(Worker_T w =0; w<k;++w){
+		//availibilty 
+		if(!avail[day][w]|| shiftsCount[w] >= maxShifts){
+			continue;
+		}
+		bool alreadyScheduled = false; // making sure that worker is not already in the schedule for the day
+		for(size_t i = 0; i< slot;++i){
+			if(sched[day][i]==w){
+				alreadyScheduled = true;
+				break;
+			}
+		}
+		if(alreadyScheduled) continue;
+		//assign and recusrve
+		sched[day][slot] =w;
+		shiftsCount[w]++;
+		if(helper(avail, dailyNeed, maxShifts, sched, shiftsCount, day, slot+1)){
+			return true;
+		}
+		shiftsCount[w]--; //backtrack
+	}
+	return false;//no slot found basically
+}
--- wordle.cpp
+++ wordle.cpp
@@ -14,6 +14,7 @@ using namespace std;
 
 // Add prototypes of helper functions here
 
+void helper(int position, const string& in, string& current, string remaining, const set<string>&dict, set<string>& results);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -21,8 +22,65 @@ std::set<std::string> wordle(
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
+    set<string> results;
+    string current = in; //current state
+    helper(0,in,current,floating,dict,results);
+    return results;
 
 }
 
-// Define any helper functions here
+void helper(int position, const string& in, string& current, string remaining, const set<string>&dict, set<string>& results){
+    int n = in.size();
+    //if floating letter been placed down
+    if(position ==n){
+        if(remaining.empty()){
+            if(dict.find(current)!=dict.end()){
+        results.insert(current);
+        }
+        }
+        return;
+    }
+    //positioned fixed, copy and recusrve it
+    if(in[position]!= '-'){
+        current[position]= in[position];
+        helper(position+1, in, current, remaining, dict, results);
+    } else{
+        int blanks=0; //amoutn of blanks left
+        for(int i = position;i<n; i++){
+            if(in[i]=='-') blanks++;
+        }
+        //if blanks equal floating letters, place letter
+        if(blanks ==(int)remaining.size()){
+            for(int i=0; i< (int)remaining.size();i++){
+                char c = remaining[i];
+                string nestFloating = remaining;
+                nestFloating.erase(i, 1);
+                current[position]= c;
+                helper(position+1, in, current, nestFloating, dict, results);
+            }
+        } else{
+            //place a floating letter
+            for(int i= 0; i<(int)remaining.size(); i++){
+                char c = remaining[i];
+                string nestFloating = remaining;
+                nestFloating.erase(i,1);
+                current[position] =c;
+                helper(position+1, in, current, nestFloating, dict, results);
+            }
+            for(char c = 'a'; c<= 'z';c++){
+                bool isFloating = false;
+                for(char f: remaining){
+                    if(c==f){ 
+                        isFloating = true;
+                        break;
+                        }
+                }
+                if(isFloating) continue;
+                
+                current[position] =c;
+                helper(position+1, in, current, remaining, dict, results);
+                
+            }
+        }
+    }
+}
